9741cb73e5bf2bd57f91e0bc98cf741911e94949
jhon11390 <meljhon2@gmail.com>
2020-07-27 02:06:19 -0500

    Crea el reto de formulario

--- app.js
+++ app.js
@@ -1,42 +1,37 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const path = require('path');
-const User = require("./user");
-
-mongoose.connect(process.env.MONGODB_URL ||'mongodb://localhost:27017/mongo-3', { useNewUrlParser: true });
-
+const express = require('express');
 const app = express();
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/mongo-1', { useNewUrlParser: true });
+mongoose.connection.on("error", function(e){ console.error(e); });
+app.use(express.urlencoded());
 app.set('view engine', 'pug');
 app.set('views', 'views');
-app.use(express.urlencoded({ extended: true }));
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get("/", async (req, res) => {
-  const users = await User.find();
-  res.render("index", { users: users });
+// definimos el schema
+var schema = mongoose.Schema({ name: String, email: String, password: String });
+// definimos el modelo
+var User = mongoose.model("User", schema);
+
+app.get('/', (req, res) => {
+  User.find(function(err, users) {
+    console.log(users)
+    if (err) return console.error(err);
+    res.render('index', { users: users });
+  });
 });
 
-app.get("/register", (req, res) => {
-  res.render("new");
+app.get('/register', (req, res) => {
+  res.render('form');
 });
 
-app.post("/register", async (req, res) => {
-  const name = req.body.name;
-  const email = req.body.email;
-  const password = req.body.password;
-
-  const data = {
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  };
-
-  try {
-    const user = await User.create(data);
-  } catch (e) {
-    console.error(e);
-  }
-  res.redirect("/");
+app.post('/register', (req, res) => {
+  var user = new User(req.body);
+  user.save(function(err){
+    if (err) return console.error(err);
+  })
+  console.log(user)
+  res.redirect('/');
 });
 
-app.listen(3000, () => console.log("Listening on port 3000 ..."));
+app.listen(3000, () => console.log('listening 3000'));
