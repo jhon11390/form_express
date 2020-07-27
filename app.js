e81cae3edd654a5d0a73a4391d1c806e2bdbf623
jhon11390 <meljhon2@gmail.com>
2020-07-26 20:34:17 -0500

    Crea fomurlario express

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require('path');
+const User = require("./user");
+
+mongoose.connect(process.env.MONGODB_URL || "mongodb://localhost:27017/registrations", { useNewUrlParser: true });
+
+const app = express();
+app.set('view engine', 'pug');
+app.set('views', 'views');
+app.use(express.urlencoded({ extended: true }));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get("/", async (req, res) => {
+  const users = await User.find();
+  res.render("index", { users: users });
+});
+
+app.get("/register", (req, res) => {
+  res.render("new");
+});
+
+app.post("/register", async (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const data = {
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  };
+
+  try {
+    const user = await User.create(data);
+  } catch (e) {
+    console.error(e);
+  }
+  res.redirect("/");
+});
+
+app.listen(3000, () => console.log("Listening on port 3000 ..."));
---
+++ user.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const schema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+// hashes the password
+schema.pre("save", function (next) {
+  bcrypt.hash(this.password, 10, (err, hash) => {
+    if (err) {
+      return next(err);
+    }
+    this.password = hash;
+    next();
+  });
+});
+
+module.exports = mongoose.model("User", schema);
